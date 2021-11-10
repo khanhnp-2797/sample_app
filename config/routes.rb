484cf89fe047c7f0b89d0c1a6381eff42d18fc89
KHANHNP <nguyen.phi.khanh@sun-asterisk.com>
2021-11-10 17:12:34 +0700

    Mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home" 
+    get "/home", to: "static_pages#home"
+    get "/help", to: "static_pages#help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
