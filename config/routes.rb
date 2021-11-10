fd1e07605ce4851719f9549465c0e04b857f9915
KHANHNP <nguyen.phi.khanh@sun-asterisk.com>
2021-11-10 17:12:34 +0700

    Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
     post "/signup", to: "users#create"
     resources :users, only: %i(new create)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete  "/logout", to: "sessions#destroy"
   end
 end
