261ed43b4f7b40f0dfa2be48e7dd1e4e1cb72fe5
KhanhNguyen <nguyen.phi.khanh@sun-asterisk.com>
2021-11-20 00:54:10 +0700

    Reset Password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home" 
     get "/home", to: "static_pages#home"
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
     post "/signup", to: "users#create"
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, except: %i(index show destroy)
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete  "/logout", to: "sessions#destroy"
